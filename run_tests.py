0b06556148194b77e62d28c5bcc8b7c79bc44334
pedrohmx <pedrohx@yahoo.com.br>
2022-08-01 15:25:19 -0300

    🧪 Setup for input

--- run_tests.py
+++ run_tests.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-SAMPLE_SIZES = [10,]
-INPUT_FILES  = [f'data/{x}.txt' for x in SAMPLE_SIZES]
-OUTPUT_FILES = [f'results/{x}.txt' for x in SAMPLE_SIZES]
+SAMPLE_SIZES = [10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 650, 800, 1000, 1500]
+INPUT_FILES  = [f'data/Entrada {x}.txt' for x in SAMPLE_SIZES]
+OUTPUT_FILES = [f'results/{x}.csv' for x in SAMPLE_SIZES]
 
-#EXEC = './bin/release/t4'
-EXEC = './bin/debug/t4'
+EXEC = './bin/release/t4'
+# EXEC = './bin/debug/t4'
+# EXEC = 'echo'
 RUNS = '11'
 
 for i, (input_file, output_file) in enumerate(zip(INPUT_FILES, OUTPUT_FILES)):
@@ -19,6 +20,7 @@ for i, (input_file, output_file) in enumerate(zip(INPUT_FILES, OUTPUT_FILES)):
     subprocess.run(args=[
         EXEC, input_file, "-r", RUNS, "-o", output_file
     ])
+    #break
 
 print('------------------------------------------------------------------------')
 print('Done.')
