04861498dcfac3a852be3654d343b8d2c4fa1c3f
pedrohmx <pedrohx@yahoo.com.br>
2022-08-01 09:27:19 -0300

    ✨ Added dijkstra

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 //         1_000_000
 #define INF 1000000
 
-void print_mat(int *mat, size_t size) {
+void print_mat(const int* const mat, size_t size) {
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
             std::cout << mat[i*size+j] << ' ';
@@ -31,6 +31,68 @@ void floyd_warshall(int *mat, const size_t &size) {
     }
 }
 
+int min_distance_index(int *dist, bool *visited, const size_t &size) {
+    int min = INF, min_index;
+    for (size_t i = 0; i < size; ++i) {
+        if (visited[i] == false && dist[i] <= min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void dijkstra(
+    int const *og_mat,
+    int  *dist,
+    bool *visited,
+    const int &src,
+    const size_t &size
+) {
+    // Initialize distances and visited
+    for (size_t i = 0; i < size; ++i) {
+        dist[i] = INF;
+        visited[i] = false;
+    }
+    // distance to self
+    dist[src] = 0;
+    for (size_t i = 0; i < size-1; ++i) {
+        int u = min_distance_index(dist, visited, size);
+        visited[u] = true;
+        
+
+        for (size_t v = 0; v < size; v++) {
+            if (  !visited[v]
+                && og_mat[u*size+v]
+                && dist[u] != INF
+                && dist[u] + og_mat[u*size+v] < dist[v]
+            ) {
+                dist[v] = dist[u] + og_mat[u*size+v];
+            }
+        }
+    }
+}
+
+void dijkstra_wrapper(
+    int const *og_mat,
+    int  *dmat,
+    bool *visited,
+    const size_t &size
+) {
+    // Initialize
+    int *dist;
+    for (size_t i = 0; i < size; ++i) {
+        dist = dmat + (size * i);
+        // for (size_t j = 0; j < size; ++j) {
+        //     visited[j] = false;
+        //     dist[j] = INF;
+        // }
+        // dist[i] = 0;
+        // call dijkstra
+        dijkstra(og_mat, dist, visited, i, size);
+    }
+}
+
 auto main(int argc, char *argv[]) -> int {
     using std::cout;
     using std::cerr;
@@ -41,7 +103,6 @@ auto main(int argc, char *argv[]) -> int {
     program.add_argument("inputfile")
         //.help("File containing the data to build the trees")
         .required();
-
 
     program.add_argument("-r")
         .help("Number of runs")
@@ -76,6 +137,7 @@ auto main(int argc, char *argv[]) -> int {
     int *mat;
     // Dijkstra matrix
     int *dmat;
+    bool *visited;
     // Floyd-Warshall matrix
     int *fmat;
 
@@ -94,6 +156,7 @@ auto main(int argc, char *argv[]) -> int {
         mat  = new int[len];
         dmat = new int[len];
         fmat = new int[len];
+        visited = new bool[len];
 
         int count = 0;
         while (ifs >> value && count < len) {
@@ -119,10 +182,12 @@ auto main(int argc, char *argv[]) -> int {
         cout << "Running [" << r+1 << '/' << runs << "]" << endl; //cout.flush();
 
         // Init
-        for (size_t i = 0; i < len; i++) {
-            dmat[i] = mat[i];
-        }
+        // prepare dijkstra matrix
+        // for (size_t i = 0; i < len; i++) {
+        //     dmat[i] = mat[i];
+        // }
 
+        // prepare floyd-warshall matrix
         for (size_t i = 0; i < size; i++) {
             for (size_t j = 0; j < size; j++) {
                 auto temp = mat[i*size+j];
@@ -134,14 +199,21 @@ auto main(int argc, char *argv[]) -> int {
             }
         }
 
+        cout << "--- Floyd Warshall ---" << endl;
         auto floyd_warshall_t1 = clock::now();
         floyd_warshall(fmat, size);
         auto floyd_warshall_t2 = clock::now();
-
-        cout << "--- Floyd Warshall ---" << endl;
         print_mat(fmat, size);
-        
-        // std::chrono::duration<double, std::milli> span = t2 - t1;
+        std::chrono::duration<double, std::nano> fw_span = floyd_warshall_t2 - floyd_warshall_t1;
+        cout << fw_span.count() << "ns" << endl;
+
+        cout << "--- Dijkstra ---" << endl;
+        auto dijkstra_t1 = clock::now();
+        dijkstra_wrapper(mat, dmat, visited, size);
+        auto dijkstra_t2 = clock::now();
+        print_mat(dmat, size);
+        std::chrono::duration<double, std::nano> d_span = dijkstra_t2 - dijkstra_t1;
+        cout << d_span.count() << "ns" << endl;
 
         // COLNAME | OTHERCOL
         // ofs_output   << 123
